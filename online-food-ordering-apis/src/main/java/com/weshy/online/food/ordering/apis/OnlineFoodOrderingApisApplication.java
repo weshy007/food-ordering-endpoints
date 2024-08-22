168e173d92defb0a10b562dffddf184cf7ad4b85
Weshy Joe <josephwaweru96@gmail.com>
2024-08-22 18:17:29 +0300

    initial commit

---
+++ online-food-ordering-apis/src/main/java/com/weshy/online/food/ordering/apis/OnlineFoodOrderingApisApplication.java
@@ -0,0 +1,13 @@
+package com.weshy.online.food.ordering.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderingApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderingApisApplication.class, args);
+	}
+
+}
---
+++ online-food-ordering-apis/src/test/java/com/weshy/online/food/ordering/apis/OnlineFoodOrderingApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weshy.online.food.ordering.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
