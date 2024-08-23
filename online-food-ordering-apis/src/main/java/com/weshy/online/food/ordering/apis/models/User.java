04ce29ea44b925be788d708cfc5dcdf1ed5ccdd9
Weshy Joe <josephwaweru96@gmail.com>
2024-08-23 14:47:29 +0300

    complete user entity

---
+++ online-food-ordering-apis/src/main/java/com/weshy/online/food/ordering/apis/models/User.java
@@ -0,0 +1,40 @@
+package com.weshy.online.food.ordering.apis.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.weshy.online.food.ordering.apis.dto.RestaurantDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String full_name;
+    private String email;
+    private String password;
+
+    private USER_ROLE role;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    private List<RestaurantDTO>favorites = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+
+
+}
