84c892b8112a37ec09d7b069e42ce73ece65559c
Weshy Joe <josephwaweru96@gmail.com>
2024-08-23 17:02:57 +0300

    app config -> jwt

---
+++ online-food-ordering-apis/src/main/java/com/weshy/online/food/ordering/apis/config/AppConfig.java
@@ -0,0 +1,61 @@
+package com.weshy.online.food.ordering.apis.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize -> Authorize
+                        .requestMatchers("/api/admin/**").hasAnyRole("RESTAURANT_OWNER","ADMIN")
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf->csrf.disable())
+                .cors(cors->cors.configurationSource(corsConfigurationSource()));
+
+        return http.build();
+    }
+
+    // Change the return type to CorsConfigurationSource
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:8000",
+                "http://localhost:3000"
+        ));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setExposedHeaders(Arrays.asList("Authorization"));
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;  // Return a CorsConfigurationSource
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ online-food-ordering-apis/src/main/java/com/weshy/online/food/ordering/apis/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.weshy.online.food.ordering.apis.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "8yr483f8t487ewy45hf8g8f75t67iwi8hurh7vc804e743gvuygth4gv843gt8g4863vt463"; // Random string
+    public static final String JWT_HEADER = "Authorization";
+}
