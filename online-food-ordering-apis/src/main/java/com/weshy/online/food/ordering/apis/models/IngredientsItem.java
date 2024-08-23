f4952130448aa30e5c97e5cf6d6315f9d54e29db
Weshy Joe <josephwaweru96@gmail.com>
2024-08-23 14:54:12 +0300

    complete ingredient category and items entity

---
+++ online-food-ordering-apis/src/main/java/com/weshy/online/food/ordering/apis/models/IngredientsItem.java
@@ -0,0 +1,29 @@
+package com.weshy.online.food.ordering.apis.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class IngredientsItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    private IngredientCategory ingredientCategory;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private boolean inStock = true;
+
+}
