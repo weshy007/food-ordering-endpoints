96c63d3d534d7acbb4fcd5448a75e80ad8b34a4f
Weshy Joe <josephwaweru96@gmail.com>
2024-09-02 19:17:25 +0300

    restaurant interface service

---
+++ online-food-ordering-apis/src/main/java/com/weshy/online/food/ordering/apis/service/RestaurantService.java
@@ -0,0 +1,29 @@
+package com.weshy.online.food.ordering.apis.service;
+
+import com.weshy.online.food.ordering.apis.dto.RestaurantDTO;
+import com.weshy.online.food.ordering.apis.models.Restaurant;
+import com.weshy.online.food.ordering.apis.models.User;
+import com.weshy.online.food.ordering.apis.request.CreateRestaurantRequest;
+
+import java.util.List;
+
+public interface RestaurantService {
+    
+    public Restaurant createRestaurant(CreateRestaurantRequest req, User user);
+    public Restaurant updateRestaurant(Long restaurantId, CreateRestaurantRequest updatedRestaurant) throws Exception;
+
+    public void deleteRestaurant(Long restaurantId) throws Exception;
+
+    public List<Restaurant> getAllRestaurants();
+
+    public List<Restaurant> searchRestaurant();
+
+    public Restaurant findRestaurantById(Long restaurantId) throws Exception;
+
+    public Restaurant getRestaurantByUserId(Long userId) throws Exception;
+
+    public RestaurantDTO addToFavorites(Long restaurantId, User user) throws Exception;
+
+    public Restaurant updateRestaurantStatus(Long id) throws Exception;
+
+}
