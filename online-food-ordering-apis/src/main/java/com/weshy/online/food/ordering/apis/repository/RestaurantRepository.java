5813d6e3cc4a9c9cd5cca7fc83b8a6aa1044ca2a
Weshy Joe <josephwaweru96@gmail.com>
2024-09-02 19:25:30 +0300

    restaurant repository

---
+++ online-food-ordering-apis/src/main/java/com/weshy/online/food/ordering/apis/repository/RestaurantRepository.java
@@ -0,0 +1,17 @@
+package com.weshy.online.food.ordering.apis.repository;
+
+import com.weshy.online.food.ordering.apis.models.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    @Query("SELECT r FROM Restaurant r WHERE lower(r.name) LIKE lower(concat('%', :query, '%')) " +
+            "OR lower(r.cuisineType) LIKE lower(concat('%', :query, '%'))")
+    List<Restaurant> findBySearchQuery(String query);
+
+    Restaurant findByOwnerId(Long userId);
+
+}
